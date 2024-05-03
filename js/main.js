0c0a7009c65b58cdca7b014ff7b9b1458bf957d1
lectarica <lectarica@gmail.com>
2024-05-04 03:38:49 +0900

    update

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@ $(window).on("load", function () {
     const FPS = 5;
     const INTERVAL = 1000 / FPS;
 });
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    const content = document.getElementById('page-content');
+    content.style.opacity = 1; 
+});
---
+++ js/script.js
@@ -0,0 +1,4 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const content = document.getElementById('page-content');
+    content.style.opacity = 1; // ページのコンテンツの透明度を1に設定してフェードイン
+});
