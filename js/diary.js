3cfb3df17532bdc9493f1f00663e5d01a247463b
lectarica <lectarica@gmail.com>
2025-03-23 02:49:14 +0900

    update

--- js/diary.js
+++ js/diary.js
@@ -57,9 +57,9 @@ document.addEventListener('DOMContentLoaded', function() {
         const yyyy = currentDate.getFullYear();
         const mm = String(currentDate.getMonth()+1).padStart(2, '0');
         const dd = String(currentDate.getDate()).padStart(2, '0');
-        const prevDateStr = `diaries/${yyyy}-${mm}-${dd}-diary.txt`;
+        const prevDateStr = `${yyyy}-${mm}-${dd}`;
         dateInput.value = prevDateStr;
-        loadDiaryEntry(prevDateStr);
+        loadDiaryEntry(`diaries/${prevDateStr}-diary.txt`);
     });
 
     document.getElementById('next-day').addEventListener('click', function() {
@@ -68,9 +68,9 @@ document.addEventListener('DOMContentLoaded', function() {
         const yyyy = currentDate.getFullYear();
         const mm = String(currentDate.getMonth()+1).padStart(2, '0');
         const dd = String(currentDate.getDate()).padStart(2, '0');
-        const nextDateStr = `diaries/${yyyy}-${mm}-${dd}-diary.txt`;
+        const nextDateStr = `${yyyy}-${mm}-${dd}`;
         dateInput.value = nextDateStr;
-        loadDiaryEntry(nextDateStr);
+        loadDiaryEntry(`diaries/${nextDateStr}-diary.txt`);
     });
 });
 
